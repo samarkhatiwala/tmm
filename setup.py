dedd128b040e395be597ffd2717c2d532d94def3
Jamie Carr <jammehcarr@gmail.com>
2025-08-01 21:51:58 +0100

    Added tmmlib and tmm4py packages for local install

---
+++ setup.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python3
+
+"""
+TMM: Transport Matrix Method Library
+====================================
+
+The Transport Matrix Method (TMM) library provides efficient numerical methods
+for ocean circulation modeling and biogeochemical simulations.
+"""
+
+import re
+import os
+import sys
+import shlex
+import shutil
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+try:
+    from setuptools.command.bdist_wheel import bdist_wheel as _bdist_wheel
+except ImportError:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+init_py = """\
+def get_tmm_dir():
+    import os
+    return os.path.dirname(__file__)
+
+
+def get_config():
+    conf = {}
+    conf['TMM_DIR'] = get_tmm_dir()
+    return conf
+"""
+
+main_py = """\
+if __name__ == "__main__":
+    import sys
+    if "--prefix" in sys.argv:
+        from . import get_tmm_dir
+        print(get_tmm_dir())
+        del get_tmm_dir
+    del sys
+"""
+
+metadata = {
+    "provides": ["tmmlib"],
+    "zip_safe": False,
+}
+
+
+def bootstrap():
+
+    TMM_DIR = os.path.abspath(os.getcwd())
+    os.environ["TMM_DIR"] = TMM_DIR
+    sys.path.insert(0, os.path.join(TMM_DIR, "config"))
+
+    pkgdir = os.path.join("config", "pypi")
+    os.makedirs(pkgdir, exist_ok=True)
+    for pyfile, contents in (
+        ("__init__.py", init_py),
+        ("__main__.py", main_py),
+    ):
+        with open(os.path.join(pkgdir, pyfile), "w") as fh:
+            fh.write(contents)
+
+
+def has_existing_petsc_dir():
+    if "PETSC_DIR" in os.environ:
+        petsc_dir = os.environ["PETSC_DIR"]
+        return os.path.exists(petsc_dir)
+    return False
+
+
+def build(dry_run=False):
+    print("DEBUG: Starting TMM build")
+    log.info("TMM: build")
+
+    import petsc
+
+    petsc_dir = petsc.get_petsc_dir()
+    os.environ["PETSC_DIR"] = petsc_dir
+    print(f"DEBUG: PETSC_DIR set to: {petsc_dir}")
+    log.info("PETSC_DIR set to: %s" % petsc_dir)
+
+    # Run TMM build
+    if dry_run:
+        return
+    make = shutil.which("make")
+    command = [make, "all"]
+    status = os.system(" ".join(command))
+    if status != 0:
+        raise RuntimeError(status)
+
+
+def install(prefix, dry_run=False):
+    log.info("TMM: install")
+
+    if dry_run:
+        return
+    make = shutil.which("make")
+    command = [make, "install", "PREFIX=" + prefix]
+    status = os.system(" ".join(command))
+    if status != 0:
+        raise RuntimeError(status)
+
+
+class context(object):
+    def __init__(self):
+        self.sys_argv = sys.argv[:]
+        self.wdir = os.getcwd()
+
+    def enter(self):
+        del sys.argv[1:]
+        tdir = os.environ["TMM_DIR"]
+        os.chdir(tdir)
+        return self
+
+    def exit(self):
+        sys.argv[:] = self.sys_argv
+        os.chdir(self.wdir)
+
+
+class cmd_install(_install):
+
+    def initialize_options(self):
+        _install.initialize_options(self)
+
+    def finalize_options(self):
+        _install.finalize_options(self)
+        self.install_lib = self.install_platlib
+        self.install_libbase = self.install_lib
+        self.old_and_unmanageable = True
+
+    def run(self):
+        root_dir = os.path.abspath(self.install_lib)
+        prefix = os.path.join(root_dir, "tmmlib")
+
+        ctx = context().enter()
+        try:
+            if has_existing_petsc_dir():
+                log.info(
+                    "DEBUG: PETSC_DIR already found in environment. Skipping build and install of TMM"
+                )
+            else:
+                build(self.dry_run)
+                install(prefix, self.dry_run)
+        finally:
+            ctx.exit()
+
+        self.outputs = []
+        if os.path.exists(prefix):
+            for dirpath, dirnames, filenames in os.walk(prefix):
+                for fn in filenames:
+                    self.outputs.append(os.path.join(dirpath, fn))
+
+        _install.run(self)
+
+    def get_outputs(self):
+        outputs = getattr(self, "outputs", [])
+        outputs += _install.get_outputs(self)
+        return outputs
+
+
+class cmd_bdist_wheel(_bdist_wheel):
+
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+        self.build_number = None
+
+    def get_tag(self):
+        plat_tag = super().get_tag()[-1]
+        return (self.python_tag, "none", plat_tag)
+
+
+def version():
+    # Read version from include/tmm/tmmversion.h
+    version_re = {
+        "major": re.compile(r"#define\s+TMM_VERSION_MAJOR\s+(\d+)"),
+        "minor": re.compile(r"#define\s+TMM_VERSION_MINOR\s+(\d+)"),
+        "subminor": re.compile(r"#define\s+TMM_VERSION_SUBMINOR\s+(\d+)"),
+    }
+    tmmversion_h = os.path.join("include", "tmm", "tmmversion.h")
+    if os.path.exists(tmmversion_h):
+        try:
+            with open(tmmversion_h, "r") as f:
+                data = f.read()
+            major = int(version_re["major"].search(data).groups()[0])
+            minor = int(version_re["minor"].search(data).groups()[0])
+            subminor = int(version_re["subminor"].search(data).groups()[0])
+            return "%d.%d.%d" % (major, minor, subminor)
+        except:
+            pass
+
+    # default version if header file doesn't exist or can't be read
+    return "1.0.0"
+
+
+description = __doc__.split("\n")[1:-1]
+del description[1:3]
+
+classifiers = """
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+Operating System :: POSIX
+Programming Language :: C
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Scientific/Engineering
+Topic :: Software Development :: Libraries
+"""
+
+bootstrap()
+setup(
+    name="tmmlib",
+    version=version(),
+    description=description.pop(0),
+    long_description="\n".join(description),
+    long_description_content_type="text/x-rst",
+    classifiers=classifiers.split("\n")[1:-1],
+    keywords=["TMM", "Ocean", "Transport", "Matrix"],
+    platforms=["POSIX"],
+    license="MIT",
+    url="https://github.com/example/tmm",
+    download_url=None,
+    author="TMM Team",
+    author_email="tmm-maint@example.com",
+    maintainer="TMM Team",
+    maintainer_email="tmm-maint@example.com",
+    packages=["tmmlib"],
+    package_dir={"tmmlib": "config/pypi"},
+    cmdclass={
+        "install": cmd_install,
+        "bdist_wheel": cmd_bdist_wheel,
+    },
+    install_requires=[],
+    setup_requires=[],
+    **metadata,
+)
--- src/binding/tmm4py/setup.py
+++ src/binding/tmm4py/setup.py
@@ -1,64 +1,148 @@
+#!/usr/bin/env python3
+
+"""
+TMM4Py: Python Interface for Transport Matrix Method Library
+============================================================
+
+TMM4Py provides Python bindings for the Transport Matrix Method (TMM) library,
+enabling efficient numerical methods for ocean circulation modeling and
+biogeochemical simulations through a Python interface.
+"""
+
+import re
+import os
+import sys
+import numpy
+import warnings
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import os
-import numpy
-import sys
-import warnings
+import site
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+metadata = {
+    "provides": ["tmm4py"],
+    "zip_safe": False,
+}
+
+
+description = __doc__.split("\n")[1:-1]
+del description[1:3]
+
+classifiers = """
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+Operating System :: POSIX
+Programming Language :: C
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Scientific/Engineering
+Topic :: Software Development :: Libraries
+"""
+
+
+def version():
+    # Read version from include/tmm/tmmversion.h
+    version_re = {
+        "major": re.compile(r"#define\s+TMM_VERSION_MAJOR\s+(\d+)"),
+        "minor": re.compile(r"#define\s+TMM_VERSION_MINOR\s+(\d+)"),
+        "subminor": re.compile(r"#define\s+TMM_VERSION_SUBMINOR\s+(\d+)"),
+    }
+    tmmversion_h = os.path.join("..", "..", "..", "include", "tmm", "tmmversion.h")
+    if os.path.exists(tmmversion_h):
+        try:
+            with open(tmmversion_h, "r") as f:
+                data = f.read()
+            major = int(version_re["major"].search(data).groups()[0])
+            minor = int(version_re["minor"].search(data).groups()[0])
+            subminor = int(version_re["subminor"].search(data).groups()[0])
+            return "%d.%d.%d" % (major, minor, subminor)
+        except:
+            pass
+
+    # default version if header file doesn't exist or can't be read
+    return "1.0.0"
+
+
+def get_site_package_dir(package_name):
+    """Find out where the final package will be installed in site-packages"""
+    return os.path.join(site.getsitepackages()[0], package_name)
+
 
 # Get PETSc and TMM paths from environment variables
-if 'PETSC_DIR' in os.environ:
-  petsc_dir = os.environ['PETSC_DIR']
+if "PETSC_DIR" in os.environ and os.environ["PETSC_DIR"] != "":
+    petsc_dir = os.environ["PETSC_DIR"]
 else:
-  raise RuntimeError("You must set the PETSC_DIR environment variable before building tmm4py")
+    import petsc
 
-if 'PETSC_ARCH' in os.environ:
-  petsc_arch = os.environ['PETSC_ARCH']
+    petsc_dir = petsc.get_petsc_dir()
+    os.environ["PETSC_DIR"] = petsc_dir
+
+log.info("Using PETSC_DIR: " + petsc_dir)
+
+if "PETSC_ARCH" in os.environ:
+    petsc_arch = os.environ["PETSC_ARCH"]
 else:
-  raise RuntimeError("You must set the PETSC_ARCH environment variable before building tmm4py")
+    log.error("PETSC_ARCH not set. Using default PETSC_ARCH")
+    petsc_arch = ""
 
-if 'TMM_DIR' in os.environ:
-  tmm_root = os.environ['TMM_DIR']
-  tmm_lib_path = os.path.join(tmm_root,'lib')
-  tmm_include_path = os.path.join(tmm_root,'include')
+if "TMM_DIR" in os.environ and os.environ["TMM_DIR"] != "":
+    tmm_root = os.environ["TMM_DIR"]
 else:
-  raise RuntimeError("You must set the TMM_DIR environment variable before building tmm4py")
+    import tmmlib
 
-petscvariables = os.path.join(petsc_dir,petsc_arch,'lib','petsc','conf','petscvariables')
+    tmm_root = tmmlib.get_tmm_dir()
+    os.environ["TMM_DIR"] = tmm_root
+
+log.info("Using TMM_DIR: " + tmm_root)
+
+tmm_lib_path = os.path.join(tmm_root, "lib")
+tmm_include_path = os.path.join(tmm_root, "include")
+
+petscvariables = os.path.join(
+    petsc_dir, petsc_arch, "lib", "petsc", "conf", "petscvariables"
+)
 if os.path.isfile(petscvariables):
-  fid = open(petscvariables, 'r')
-  for line in fid:
-    if line.startswith('CC '):
-      CC = line.strip().split('=')[1].strip('\n').strip()
-  fid.close()
-  if "${PETSC_DIR}" in CC:
-    CC=CC.replace('${PETSC_DIR}',petsc_dir)
+    fid = open(petscvariables, "r")
+    for line in fid:
+        if line.startswith("CC "):
+            CC = line.strip().split("=")[1].strip("\n").strip()
+    fid.close()
+    if "${PETSC_DIR}" in CC:
+        CC = CC.replace("${PETSC_DIR}", petsc_dir)
 else:
-  warnings.warn(f"File {petscvariables} not found. Trying tp determing C compiler from CC environment variable")
-  if 'CC' in os.environ:
-    CC=os.environ["CC"]
-  else:
-    raise RuntimeError(f"Environment variable CC not set. Cannot determine C compiler")
+    warnings.warn(
+        f"File {petscvariables} not found. Trying tp determing C compiler from CC environment variable"
+    )
+    if "CC" in os.environ:
+        CC = os.environ["CC"]
+    else:
+        raise RuntimeError(
+            f"Environment variable CC not set. Cannot determine C compiler"
+        )
 
-os.environ["CC"]=CC
+os.environ["CC"] = CC
 
-include_paths=[
-  os.path.join(petsc_dir, "include"),
-  os.path.join(petsc_dir, petsc_arch, "include"),
-  tmm_include_path
+include_paths = [
+    os.path.join(petsc_dir, "include"),
+    os.path.join(petsc_dir, petsc_arch, "include"),
+    tmm_include_path,
 ]
 
-# Define the extension module
 extensions = [
     Extension(
         name="tmm4py",
         sources=["tmm4py.pyx"],
-        define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_9_API_VERSION')],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_9_API_VERSION")],
         include_dirs=[
             numpy.get_include(),
             os.path.join(petsc_dir, "include"),
             os.path.join(petsc_dir, petsc_arch, "include"),
             tmm_include_path,
-            tmm_lib_path
+            tmm_lib_path,
         ],
         library_dirs=[
             os.path.join(petsc_dir, petsc_arch, "lib"),
@@ -67,10 +151,35 @@ extensions = [
         libraries=["petsc", "tmm"],
         extra_compile_args=["-fPIC"],
         extra_link_args=[
-            "-Wl,-rpath," + os.path.join(petsc_dir, petsc_arch, "lib"),
-            "-Wl,-rpath," + tmm_lib_path,
+            "-Wl,-rpath," + os.path.join(get_site_package_dir("petsc"), "lib"),
+            "-Wl,-rpath," + os.path.join(get_site_package_dir("tmmlib"), "lib"),
         ],
     )
 ]
 
-setup(name="TMMInterface", ext_modules=cythonize(extensions,language_level="3",include_path=include_paths))
+
+setup(
+    name="tmm4py",
+    version=version(),
+    description=description.pop(0),
+    long_description="\n".join(description),
+    long_description_content_type="text/x-rst",
+    classifiers=classifiers.split("\n")[1:-1],
+    keywords=["TMM", "Ocean", "Transport", "Matrix", "Python", "Bindings"],
+    platforms=["POSIX"],
+    license="",
+    url="https://github.com/samarkhatiwala/tmm",
+    download_url=None,
+    author="TMM Team",
+    author_email="tmm-maint@example.com",
+    maintainer="TMM Team",
+    maintainer_email="tmm-maint@example.com",
+    ext_modules=cythonize(extensions, language_level="3", include_path=include_paths),
+    **metadata,
+    install_requires=[
+        "petsc4py",
+        "tmmlib @ file:///home/jamie/repos/tmm_repo/tmm",
+        "mpich",
+    ],
+    setup_requires=[],
+)
