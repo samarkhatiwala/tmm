08ca0fee1a249dd4240895781933f7d46d48c195
Samar Khatiwala <samkat6@gmail.com>
2025-09-11 08:16:56 +0900

    On branch main Changes to be committed:   modified:   include/tmm/tmmversion.h

--- include/tmm/tmmversion.h
+++ include/tmm/tmmversion.h
@@ -1,5 +1,5 @@
 #define TMM_VERSION_MAJOR      3
-#define TMM_VERSION_MINOR      0
+#define TMM_VERSION_MINOR      1
 #define TMM_VERSION_SUBMINOR   0
-#define TMM_RELEASE_DATE       "June 29, 2025"
-#define TMM_VERSION_DATE       "June 29, 2025"
+#define TMM_RELEASE_DATE       "Sept 11, 2025"
+#define TMM_VERSION_DATE       "Sept 11, 2025"
